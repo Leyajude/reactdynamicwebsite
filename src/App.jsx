ea5a2f0a2a6ebea69c1559deed36884de0647aa8
Leya jude <leyajude3@gmail.com>
2024-02-15 17:47:43 +0530

    inital

---
+++ src/App.jsx
@@ -0,0 +1,237 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg bg-black ">
+        <div class="container">
+          <a class="navbar-brand text-white me-5" href="#">DAMIEN</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item me-5">
+                <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item me-5 ">
+                <a class="nav-link text-white" href="#">About me</a>
+              </li>
+              <li class="nav-item me-5">
+                <a class="nav-link text-white" href="#">Portfolio</a>
+              </li>
+              <li class="nav-item me-5">
+                <a class="nav-link text-white" href="#">Services</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+     <div className='cc bg-black'>
+     <div className='container  bg-black'>
+        <div className='row row1 '>
+        <div className='col-lg-1'></div>
+
+          <div className='col-lg-5'>
+            <p className='p1 text-white mt-3'>Stunning Photography by</p>
+            <h2 className='p1 text-white'>DAMIEN BRAUN</h2>
+          </div>
+          <div className='col-lg-1'></div>
+          <div className='col-lg-5'>
+            <h2 className='p1 text-white mt-3'>Let's</h2>
+            <h1 className='p1 text-white mt-3'> Work Together</h1>
+          </div>
+
+        </div>
+        <div className='row row2'>
+          <div className='col-lg-12 '>
+            <img className='pp' src="./images/Sub Container.png" alt="" />
+          </div>
+        </div>
+        <div className='row row3 mt-4'>
+          <div className='col-lg-1'></div>
+
+          <div className='col-lg-5'>
+            <h2 className='p1 text-white'>I AM DAIMEN</h2>
+            <img className='pp' src="./images/Image (16).png" alt="" />
+          </div>
+          <div className='col-lg-5 mt-5 '>
+            <h5 className='p1 text-white'>Indroduction</h5>
+            <p className='p2'>My journey as a photographer has been a lifelong quest to capture the extraordinary in the ordinary, to freeze fleeting moments in time, and to share the world's beauty as I see it. Based in the enchanting landscapes of the USA, I find inspiration in every corner of this diverse and vibrant country. Join me as we embark on a visual odyssey, where each photograph tells a story, and every frame is a piece of my heart.</p>
+            <h5 className='mt-5 text-white'>Contact information</h5>
+            <span>Email</span>
+            <br />
+            <span>leyaghjkkk@gmail.com</span>
+            <h5 className=' text-white'>phone number</h5>
+            <span>925852145555</span>
+            <img className='pp mt-4' src="./images/Container (5).png" alt="" />
+          </div>
+          <div className='col-lg-1'></div>
+
+        </div>
+        <div className='row row4 mt-3'>
+          <div className='col-lg-1'></div>
+          <div className='col-lg-5'>
+          <h2 className='text-white'>MY PHOTOGRAPHY SERVICES</h2>
+          <h4 className='mt-3 text-white'>EVENTS</h4>
+          <p className='p2'>Our event photography service is dedicated to capturing the magic of your special occasions. Whether it's a wedding, corporate event, or milestone celebration, we're there to document every heartfelt moment. We blend into the background, ensuring natural and candid shots that reflect the emotions of the day.</p>
+          <h6 className='hh text-white'>Service Highlights</h6>
+          <p className='p2'>Coverage for weddings, parties, corporate functions, and more</p>
+          <br />
+          <p className='p2'>Skilled photographers who know how to seize the moment.</p>
+          <br />
+          <p className='p2'>A mix of candid and posed shots for a comprehensive story.</p>
+          <br />
+          <p className='p2'>Quick turnaround for you to relive the day's highlights.</p>
+          <br />
+          </div>
+          
+
+          <div className='col-lg-5'>
+          <img  className='pp mt-5' src="./images/Image (17).png" alt="" />
+          </div>
+          <div className='col-lg-1'></div>
+
+        </div>
+        <div className='row row8 mt-3'>
+          <div className="col-lg-1"></div>
+          <div className='col-lg-10'>
+            <img className='pt' src=".\images\Container (8).png" alt="" />
+          </div>
+          <div className="col-lg-1"></div>
+
+        </div>
+        <br />
+        <div className="row">
+        <h3 className='tt text-center mt-4'>Protrait Photograpgy</h3>
+
+          <div className="col-lg-1"></div>
+
+          <div className="col-lg-3 mt-3">
+            
+            <img className='pp' src=".\images\Card (31).png" alt="" />
+          </div>
+          <div className="col-lg-3 mt-3">
+          <img className='pp ' src=".\images\Card (32).png" alt="" />
+
+          </div>
+          <div className="col-lg-3 mt-3">
+          <img className='pp' src=".\images\Card (33).png" alt="" />
+
+          </div>
+          <div className="col-lg-1"></div>
+
+        </div>
+        <div className="row">
+        <h3 className='tt text-center mt-4'>Commercial Photography</h3>
+
+          <div className="col-lg-1"></div>
+
+          <div className="col-lg-3 mt-3">
+            
+            <img className='pp' src=".\images\Card (34).png" alt="" />
+          </div>
+          <div className="col-lg-3 mt-3">
+          <img className='pp ' src=".\images\Card (35).png" alt="" />
+
+          </div>
+          <div className="col-lg-3 mt-3">
+          <img className='pp' src=".\images\Card (36).png" alt="" />
+
+          </div>
+          <div className="col-lg-1"></div>
+
+        </div>
+
+        <h2 className='gg text-center mt-4 text-white'>Explore My photography work.</h2>
+        <div className='row row5'>
+        <div className='col-lg-1'></div>
+
+          <div className='col-lg-3'>
+            <img className='pp mt-5' src="./images/Card (25).png" alt="" />
+          </div>
+          <div className='col-lg-3'>
+          <img className='pp mt-5' src="./images/Card (26).png" alt="" />
+
+          </div>
+          <div className='col-lg-3'>
+          <img className='pt mt-5' src="./images/Card (27).png" alt="" />
+
+          </div>
+
+        </div>
+        <div className='row row6 mt-5'>
+        <div className='col-lg-1'></div>
+        <div className='col-lg-5'>
+          <h4 className='text-white mt-3'>Frequently Asked Questions</h4>
+          <p className='p2'>What type of photography do you specialize in?</p>
+         <br />
+          <p className='p2'>I specialize in [Portrait, Landscape, Event, etc.] photography, capturing moments that tell unique stories.</p>
+         <br />
+         <br />
+          <p  className='p2'>How can I book a photography session with you?</p>
+          <br />
+          <p  className='p2'>What equipment do you use for your photography?</p>
+          <br />
+          <p  className='p2'>Can I request a specific location for a </p>
+        </div>
+
+          <div className='col-lg-5 mt-5'>
+          <p className='p2'>What type of photography do you specialize in?</p>
+          <br />
+          <p  className='p2'>Can I request a specific location for a </p>
+        <br />
+        <br />
+        <p className='p2'>I specialize in [Portrait, Landscape, Event, etc.] photography, capturing moments that tell unique stories.</p>
+          <br />
+          <p  className='p2'>What equipment do you use for your photography?</p>
+<br />
+<p  className='p2'>How can I book a photography session with you?</p>
+
+          </div>
+          <div className='col-lg-1'></div>
+
+          <div></div>
+        </div>
+        <h2 className='gg text-center text-white mt-5'>WHAT MY CLIENTS SAY.</h2>
+
+        <div className='row row7 mt-3'>
+        <div className='col-lg-1'></div>
+        <div className='col-lg-3'>
+            <img className='pp mt-5' src="./images/Card (28).png" alt="" />
+          </div>
+          <div className='col-lg-3'>
+          <img className='pp mt-5' src="./images/Card (29).png" alt="" />
+
+          </div>
+          <div className='col-lg-3'>
+          <img className='pt mt-5' src="./images/Card (30).png" alt="" />
+
+          </div>
+        </div>
+       
+        <div className='row row8 mt-3'>
+          <div className='col-lg-12'>
+            <img className='pt' src=".\images\Sub Container (1).png" alt="" />
+          </div>
+        </div>
+        <div className='row'>
+          <div className="col-lg-12">
+            <img className='pp' src=".\images\Container (6).png" alt="" />
+          </div>
+        </div>
+        <div className='row'>
+          <div className="col-lg-12">
+            <img className='pp' src=".\images\Container (7).png" alt="" />
+          </div>
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
